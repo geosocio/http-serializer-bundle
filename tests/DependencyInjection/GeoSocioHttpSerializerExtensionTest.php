5f7208f5645932fe14866eb0e58ee214487b4110
David Barratt <dbarratt@wikimedia.org>
2017-07-30 18:42:10 -0400

    Add load test

--- tests/DependencyInjection/GeoSocioHttpSerializerExtensionTest.php
+++ tests/DependencyInjection/GeoSocioHttpSerializerExtensionTest.php
@@ -3,6 +3,8 @@
 namespace GeoSocio\HttpSerializerBundle\DependencyInjection;
 
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 /**
  * Extension Test
@@ -18,5 +20,31 @@ class GeoSocioHttpSerializerExtensionTest extends TestCase
         $extension = new GeoSocioHttpSerializerExtension();
 
         $this->assertEquals($alias, $extension->getAlias());
+    }
+
+    /**
+     * Test Load
+     */
+    public function testLoad()
+    {
+        $definition = $this->getMockBuilder(Definition::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        $definition->method('setArguments')
+            ->willReturnSelf();
+        $definition->method('setPublic')
+            ->willReturnSelf();
+        $definition->method('addTag')
+            ->willReturnSelf();
+
+        $container = $this->getMockBuilder(ContainerBuilder::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        $container->method('register')
+            ->willReturn($definition);
+
+        $extension = new GeoSocioHttpSerializerExtension();
+
+        $this->assertNull($extension->load([], $container));
     }
 }
