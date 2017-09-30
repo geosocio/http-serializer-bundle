b161d7767084ead2c7ef46d6ee3765172e4ede52
David Barratt <dbarratt@wikimedia.org>
2017-09-30 12:21:44 -0400

    Resolve code coverage

--- tests/DependencyInjection/Compiler/GroupResolverPassTest.php
+++ tests/DependencyInjection/Compiler/GroupResolverPassTest.php
@@ -3,6 +3,7 @@
 namespace GeoSocio\HttpSerializerBundle\DependencyInjection\Compiler;
 
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 /**
@@ -20,6 +21,22 @@ class GroupResolverPassTest extends TestCase
         $containerBuilder = $this->getMockBuilder(ContainerBuilder::class)
             ->disableOriginalConstructor()
             ->getMock();
+
+        $containerBuilder->method('has')
+            ->willReturnOnConsecutiveCalls(false, true);
+
+        $definition = $this->getMockBuilder(Definition::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $containerBuilder->method('getDefinition')
+            ->willReturn($definition);
+        $containerBuilder->method('findTaggedServiceIds')
+            ->willReturn([
+                'geosocio_http_serializer.response_group_loader' => [
+                    'geosocio_http_serializer.response_group_resolver' => [],
+                ]
+            ]);
 
         $this->assertNull($pass->process($containerBuilder));
     }
